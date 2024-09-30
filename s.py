11f14a2b2ae010f0283f077ad61d0a32eaf6e2b1
Adnan Shafi <112738018+adnanshafi99@users.noreply.github.com>
2024-09-30 23:57:48 +0800

    Removing port

--- s.py
+++ s.py
@@ -36,7 +36,7 @@ def get_prediction():
 
 # Run Flask app in a separate thread
 def run_flask():
-    app.run(port=2568)
+    app.run()
 
 # Start Flask in a separate thread
 threading.Thread(target=run_flask).start()
